40bfeb7a154777e8135682450761581bc1df0fca
James Eastham <dev@jameseastham.co.uk>
2024-09-09 12:07:14 +0100

    Refactor to separate business logic from worker code

---
+++ src/backend/src/auth.rs
@@ -0,0 +1,38 @@
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use worker::Date;
+
+use crate::Claims;
+
+pub struct AuthenticationService {
+    jwt_secret: String
+}
+
+impl AuthenticationService {
+    pub fn new (jwt_secret: String) -> Self {
+        AuthenticationService{
+            jwt_secret
+        }
+    }
+
+    pub fn generate_token_for(&self, username: String) -> std::result::Result<String, ()> {
+        let claims = Claims {
+            sub: username,
+            exp: (Date::now().as_millis() / 1000) as usize + 3600, // 1 hour expiration
+        };
+
+        let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(&self.jwt_secret.as_ref()))
+            .map_err(|e| ())?;
+
+        Ok(token)
+    }
+    
+    pub fn verify_jwt_token(&self, token: &str) -> Result<Claims, ()> {
+        tracing::info!("Verifying JWT");
+        let token_data = decode::<Claims>(token, &DecodingKey::from_secret(&self.jwt_secret.as_ref()), &Validation::default())
+            .map_err(|e|{
+                tracing::error!("{}", e);
+                ()
+            })?;
+        Ok(token_data.claims)
+    }
+}
